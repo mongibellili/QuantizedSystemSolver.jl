322e6b9e7a684fa0d42996e1f291071f856fe2e8
Documenter.jl <documenter@juliadocs.github.io>
2024-06-12 13:12:50 +0000

    build based on e59542b

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Example.jl-Documentation","page":"Example.jl Documentation","title":"Example.jl Documentation","text":"","category":"section"},{"location":"","page":"Example.jl Documentation","title":"Example.jl Documentation","text":"testfunc(x)","category":"page"},{"location":"#QuantizedSystemSolver.testfunc-Tuple{Any}","page":"Example.jl Documentation","title":"QuantizedSystemSolver.testfunc","text":"testfunc(x)\n\nReturn double the number x plus 1.\n\n\n\n\n\n","category":"method"}]
+}
